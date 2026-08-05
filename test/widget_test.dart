08c44dbfa2f9e6910ddf8877040e4e9f7a9ad00a
Ahmed Elbadri <aelbadri23@gmail.com>
2026-08-05 19:24:16 +0300

    feat: initialize Firebase and add Hive dependency for project foundation

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:pro_todo/main.dart';
+import 'package:pro_todo/todo_dev.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
